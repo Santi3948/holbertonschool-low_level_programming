e5e3ec9027ef99198b4a9ff4be43c2ae2ed019b1
Santi3948 <santiagonegreyra@gmail.com>
2021-10-01 10:18:02 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+* main - alphabet
+* Return: alphabet
+*/
+int main ()
+{
+	  char c;
+	    for (c = 'A' ; c <= 'Z' ; c++) putchar (c);
+
+	      return 0;
+}
