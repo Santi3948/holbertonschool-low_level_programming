22f744ae43c36409bc3bff2e6304c31597a1be00
Santi3948 <santiagonegreyra@gmail.com>
2021-10-03 19:51:36 -0700

    102 print

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -22,16 +22,12 @@ for (f = 50 ; f <= 57; f++)
 putchar (c);
 putchar (e);
 putchar (f);
-if (c < 55)
+if (c == 55)
 {
 putchar (44);
 putchar (32);
 }
 }
-}
-if (h == 57)
-{
-h = 49;
 }
 }
 putchar (10);
