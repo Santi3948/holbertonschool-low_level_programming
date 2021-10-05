b01c953b81efbf626ada6ac13d5dd15b3495e6d0
Santi3948 <santiagonegreyra@gmail.com>
2021-10-05 08:53:39 -0700

    alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -3,14 +3,14 @@
 
 /* betty style doc for function main goes there */
 /**
-* alphabet x10 - alphabet x10
+* print_alphabet_x10 - alphabet x10
 * Return: alphabet x10
 */
 void print_alphabet_x10(void)
 {
 char a;
 char b;
-for (b = 1; b <= 10; b ++)
+for (b = 1; b <= 10; b++)
 {
 for (a = 'a'; a <= 'z'; a++)
 {
