3567d8636dc460558dfc6c739ad4ecccee8ba298
Santi3948 <santiagonegreyra@gmail.com>
2021-10-05 09:49:59 -0700

    sign

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* print_sign - writes the character c to stdout
+* @n: The character to print
+* Return: On success 1.
+*/
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+}
+else
+if (n == 0)
+{
+_putchar('0');
+}
+else
+{
+_putchar('-');
+}
+_putchar(',');
+_putchar(' ');
+_putchar('n');
+}
