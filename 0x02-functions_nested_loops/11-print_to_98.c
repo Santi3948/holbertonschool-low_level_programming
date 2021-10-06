39fdccf14f4697f53b7b68c370864df3df7d82e0
Santi3948 <santiagonegreyra@gmail.com>
2021-10-05 18:49:20 -0700

    pritn to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -11,7 +11,7 @@ if (n >= 98)
 {
 for (a = n; a >= 98; a--)
 {
-	_putchar(a);
+	_putchar('%c',a);
 	if (n != 98)
 	{
 	_putchar(',');
@@ -23,7 +23,7 @@ else if (n < 98)
 {
 for (a = n; a <= 98; a++)
 {
-	_putchar(a);
+	_putchar('%c', a);
 	if (n != 98)
 	{
 	_putchar(',');
