da3146a1e38d7a7d6384267a3798704d046438cf
Santi3948 <santiagonegreyra@gmail.com>
2021-10-05 17:51:18 -0700

    times table

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * times_table - writes the character c to stdout
+ * Return: On success 1.
+ */
+void times_table(void)
+{
+int a,b,c,d;
+c = -1;
+for (b = 1; b <= 10; b++)
+{
+_putchar('0');
+_putchar(',');
+_putchar(' ');
+c = c + 1;
+for (a = 1; a <= 9; a++)
+{
+c = c + c;
+_putchar('c');
+_putchar(',');
+_putchar(' ');
+}
+}
+}
