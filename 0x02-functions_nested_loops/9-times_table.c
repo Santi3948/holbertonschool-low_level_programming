b24e0f88d294279b8df0520908c05239557739ff
Santi3948 <santiagonegreyra@gmail.com>
2021-10-05 18:04:12 -0700

    times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,14 +18,14 @@ for (a = 1; a <= 9; a++)
 if (a < 9)
 {
 c = c + c;
-_putchar(c);
+_putchar("%d", c);
 _putchar(',');
 _putchar(' ');
 }
 else
 {
 c = c + c;
-_putchar(c);
+_putchar("%d", c);
 }
 }
 _putchar('\n');
