8a11e9b671385f4ca3ede102628b35729c0f95e4
Santi3948 <santiagonegreyra@gmail.com>
2021-10-07 10:47:34 -0700

    asdfafds

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,18 +8,21 @@ void print_diagonal(int n)
 {
 int a;
 int b;
-if (n <= 0)
+if (n > 0)
+{
+	_putchar(92);
+	_putchar(10);
+
+for (a = 2; a <= n; a++)
+{
+	for (b = 2; b <= a; b++)
+	_putchar(' ');
+	_putchar(92);
+	_putchar(10);
+}
+}
+else
 {
 	_putchar(10);
 }
-else if (n > 0)
-{
-for (a = 1; a <= n; a++)
-{
-	for (b = 2; b <= n; b++)
-		_putchar(' ');
-	_putchar(92);
-}
-}
-_putchar(10);
 }
