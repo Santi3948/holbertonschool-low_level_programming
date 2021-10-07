14f8b3dcd6f54864ae5e78145d5e8357f6b10580
Santi3948 <santiagonegreyra@gmail.com>
2021-10-07 11:37:56 -0700

    asdf

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,29 +1,30 @@
-#include "main.h"
 #include <stdio.h>
 /**
- * FizzBuzz - writes the character c to stdout
+ * main - writes the character c to stdout
  * Return: On success 1.
  */
-int FizzBuzz(void)
+int main(void)
 {
 int a;
 for (a = 1; a <= 100; a++)
 {
-if ((a % 3) == 0)
-{
-printf("Fizz");
+	if ((a % 15) == 0)
+	{
+	printf("FizzBuzz");
+	}
+	else if ((a % 5) == 0)
+		{
+		printf("Buzz");
+		}
+		else if ((a % 3) == 0)
+		{
+		printf("Fizz");
+		}
+		else
+		{
+		printf("%d", a);
+		}
+	printf(" ");
 }
-else if ((a % 5) == 0)
-{
-printf("Buzz");
-}
-else if ((a % 15) == 0)
-{
-printf("FizzBuzz");
-}
-else
-{
-return (a);
-}
-}
+return (0);
 }
