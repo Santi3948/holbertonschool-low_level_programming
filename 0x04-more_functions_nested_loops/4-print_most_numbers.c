c75f634f29446ce20def83a59d32c356fa7d5cf4
Santi3948 <santiagonegreyra@gmail.com>
2021-10-07 09:50:10 -0700

    asdfasd

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,12 +4,13 @@
  * Return: On success 1.
  */
 void print_most_numbers(void)
-int c;
-for (c = 48; c <= 57; c++)
 {
-	if (c != 50 && c != 52)
+int pr;
+for (pr = 48; pr <= 57; c++)
+{
+	if (pr != 50 && pr != 52)
 	{
-		_putchar(c);
+		_putchar(pr);
 	}
 }
 _putchar(10);
