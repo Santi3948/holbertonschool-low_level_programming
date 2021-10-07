125e6d861d20d7ff69d03516b95c4230c6d0a65a
Santi3948 <santiagonegreyra@gmail.com>
2021-10-07 16:00:00 -0700

    doou

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@
 void print_triangle(int size)
 {
 int a, b, c, x;
-x = 1; 
+x = 1;
 if (size <= 0)
 {
 _putchar(10);
@@ -16,16 +16,16 @@ else if (size > 0)
 {
 for (a = 1; a <= size; a++)
 {
-        for (b = 1; b <= (size - x); b++)
-        {
-        _putchar(' ');
-        }
+	for (b = 1; b <= (size - x); b++)
+	{
+	_putchar(' ');
+	}
 	for (c = 1; c <= a; c++)
-        {
+	{
 	_putchar('#');
-        }
+	}
 	x = x + 1;
-        _putchar(10);
+	_putchar(10);
 }
 }
 }
