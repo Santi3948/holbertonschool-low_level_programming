18aaff7ccdfe21db717309c4e77c201f0c809179
Santi3948 <santiagonegreyra@gmail.com>
2021-10-16 06:06:04 -0700

    dou

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strcat - writes the character c to stdout
+ * @dest: The character to print
+ * @src: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, n;
+	for (n = 0 ; dest[n] != 0 ; n++);
+
+	for (i = 0 ; src[i] != '\0' ; i++)
+	{
+		dest[n] = src[i];
+		n++;
+	}
+	return dest;
+}
---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _-strncat - writes the character c to stdout
+ * @dest: The character to print
+ * @src: The character to print
+ * @n: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, c;
+	for (c = 0 ; dest[c] != 0 ; c++);
+
+	for (i = 0 ; i < n && src[i] != '\0' ; i++)
+	{
+		dest[c] = src[i];
+		c++;
+	}
+	return dest;
+}
