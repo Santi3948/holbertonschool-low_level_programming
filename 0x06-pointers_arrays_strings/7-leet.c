15d9b8a74c4d1bb9b80c1028e6bf9cfa7930c125
Santi3948 <santiagonegreyra@gmail.com>
2021-10-16 08:36:02 -0700

    aas!

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * leet - capita
+ * @s: pointer
+ * Return: s
+ *
+ */
+char *leet(char *c)
+{
+	char a[] = {'4', '3','0','7','1'};
+	char b[] = {'a', 'e','o','t','l'};
+	int i, j;
+	for (i = 0; c[i] != 0; i++)
+	for (j = 0; a[j] != 0; j++)
+	{
+		if (c[i] == b[j])
+		c[i] = a[j];
+	}
+return (c);
+}
