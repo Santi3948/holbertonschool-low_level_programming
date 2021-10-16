217e4b68bb7e4d055ba2344a5fd983248da63820
Santi3948 <santiagonegreyra@gmail.com>
2021-10-16 10:45:22 -0700

    7 leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -13,7 +13,7 @@ char d[] = {'A', 'E', 'O', 'T', 'L'};
 int i, j;
 for (i = 0; c[i] != 0; i++)
 {
-for (j = 0; a[j] != 0; j++)
+for (j = 0; b[j] != 0; j++)
 {
 if (c[i] == b[j] || c[i] == d[j])
 c[i] = a[j];
