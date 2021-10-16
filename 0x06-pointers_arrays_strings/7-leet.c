379207b09233a30539529f88fcdfff8096063a59
Santi3948 <santiagonegreyra@gmail.com>
2021-10-16 08:39:08 -0700

    aas!

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -9,11 +9,12 @@ char *leet(char *c)
 {
 	char a[] = {'4', '3','0','7','1'};
 	char b[] = {'a', 'e','o','t','l'};
+	char d[] = {'A', 'E','O','T','L'};
 	int i, j;
 	for (i = 0; c[i] != 0; i++)
 	for (j = 0; a[j] != 0; j++)
 	{
-		if (c[i] == b[j])
+		if (c[i] == b[j] || c[i] == d[j])
 		c[i] = a[j];
 	}
 return (c);
