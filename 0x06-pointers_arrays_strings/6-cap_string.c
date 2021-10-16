2c27aba798115858e9a4c7d1cecb15c1d0d6c938
Santi3948 <santiagonegreyra@gmail.com>
2021-10-16 07:42:05 -0700

    dsdsds

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  * cap_string - writes the character c to stdout
  * @s: The character to print
@@ -9,17 +8,26 @@
  */
 char *cap_string(char *s)
 {
-int i, j;
-int sep[] = {',', ';', '.', '?', '"', '(', ')', '{', '}', ' ', '\n', '\t'};
-for (i = 0; s[i] != 0; i++)
-{
-for (j = 0; sep[j] != 0; j++)
-{
-	if ((sep[j] == s[i]) && (s[i + 1] != 0) && (s[i] >= 97 && s[i] <= 122))
+	int i, j;
+	char sep[] = {',', ';', '.', '?', '"', '(', ')',
+		'{', '}', ' ', '\n', '\t'};
+	if ((s[0] >= 97) && (s[0] <= 122))
+		{
+			s[0] = s[0] - 32;
+		}
+
+	for (i = 0; s[i] != 0; i++)
 	{
-		s[i] = s[i] - 32;
+		for (j = 0; sep[j] != 0; j++)
+		{
+			if (s[i] == sep[j])
+			{
+				if (s[i + 1] >= 97 && s[i + 1] <= 122)
+				{
+					s[i + 1] = s[i + 1] - 32;
+				}
+			}
+		}
 	}
-}
-}
-return (s);
+	return (s);
 }
