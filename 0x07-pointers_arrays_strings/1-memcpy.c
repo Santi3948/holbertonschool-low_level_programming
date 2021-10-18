b59d0de2e846dfaa823473d271d0f7145dc71a03
Santi3948 <santiagonegreyra@gmail.com>
2021-10-18 09:32:41 -0700

    sds

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -2,18 +2,18 @@
 
 /**
  * _memcpy - function that copies memory area.
- * @dest: memory area 
+ * @dest: memory area
  * @src: memory area
  * @n: copies n bytes
  * Return: pointer to dest
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
-{	
+{
 	unsigned int i;
-	for (i = 0; i < n; i++)
-	{
-		dest[i] = src[i];
-	}
+for (i = 0; i < n; i++)
+{
+	dest[i] = src[i];
+}
 	dest[i] = 0;
 	return (dest);
 }
