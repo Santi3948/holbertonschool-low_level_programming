01b03f82f5c5cb158714560a0c971e0f77fa7353
Santi3948 <santiagonegreyra@gmail.com>
2021-10-20 06:36:19 -0700

    print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - writes the character c to stdout
+ * @s: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+void _print_rev_recursion(char *s)
+{
+int i = 0;
+if (s[i] != 0)
+{
+	_print_rev_recursion(&s[i + 1]);
+	_putchar(s[i]);
+} else
+{
+	return ();
+}
+}
