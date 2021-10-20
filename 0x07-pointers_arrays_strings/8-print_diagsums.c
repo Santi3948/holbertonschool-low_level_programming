56e44d2a75cff221a91011c4d6ce5ff047fe95f1
Santi3948 <santiagonegreyra@gmail.com>
2021-10-19 19:37:12 -0700

    sd

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -13,14 +13,14 @@ int i, j, k;
 int n = 0;
 int x = 0;
 for (i = 0; i < size; i++)
-{	
+{
 	k = (i * size) + i;
 	n += a[k];
 }
 for (j = 1; j <= size; j++)
-{	
+{
 	k = (j * size) - j;
-        x += a[k];
+	x += a[k];
 }
-printf("%d, %d\n", n ,x);
+printf("%d, %d\n", n, x);
 }
