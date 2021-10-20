ae6d58cde9a2c234f5129e36a03a2dbc2b94baf3
Santi3948 <santiagonegreyra@gmail.com>
2021-10-19 19:57:00 -0700

    sdds

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - finds the first occurrence of the substring
+ * @s: string to be evaluated
+ * @to: string to be evaluated
+ * Return: pointer to dest.
+ */
+
+void set_string(char **s, char *to)
+{
+*s = to;
+}
