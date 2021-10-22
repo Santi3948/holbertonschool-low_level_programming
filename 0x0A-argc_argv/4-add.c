ede9296eafa8444568ac96d24a8e891f7d249e84
Santi3948 <santiagonegreyra@gmail.com>
2021-10-22 11:25:41 -0700

    anashex

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,18 +9,18 @@
 int main(int argc, char *argv[])
 {
 	int i, j, num1, num2;
-	if (argv[1])
-	{
+if (argv[1])
+{
 	for (i = 1; i < argc; i++)
 	{
 		num1 = 0;
-		for (j = 0; argv[i][j]; j ++)
+		for (j = 0; argv[i][j]; j++)
 		{
 			if (argv[i][j] >= 48 && argv[i][j] <= 57)
 			{
 				num1 = num1 * 10;
 				num1 = num1 + (argv[i][j] - '0');
-			} else 
+			} else
 			{
 				printf("Error\n");
 				return (1);
@@ -29,7 +29,8 @@ int main(int argc, char *argv[])
 		num2 = num2 + num1;
 	}
 	printf("%d\n", num2);
-	} else
+	}
+else
 	{
 	printf("%d\n", 0);
 	}
