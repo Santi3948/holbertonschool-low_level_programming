579409f5a568c70dd996f7a1e160318128814514
Santi3948 <santiagonegreyra@gmail.com>
2021-10-24 19:02:59 -0700

    push 100

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -8,69 +8,27 @@
  */
 int main(int argc, char *argv[])
 {
-int num1, num2;
+int num1 = 0, num2;
 (void)argc;
-num1 = 0;
 if (argv[1] == 0 || argv[2] != 0)
 {
 printf("Error\n");
 return (1);
 }
-if (atoi(argv[1]) < 0)
-{
-printf("%d\n", num1);
-return (0);
-}
 num2 = atoi(argv[1]);
-if ((num2 - 25) >= 0)
-{
-num1 = num1 + 1;
-num2 = num2 - 25;
-while (num2 >= 25)
-{
-num1 = num1 + 1;
-num2 = num2 - 25;
-}
-}
-if ((num2 - 10) >= 0)
-{
-num1 = num1 + 1;
-num2 = num2 - 10;
-while (num2 >= 10)
-{
-num1 = num1 + 1;
-num2 = num2 - 10;
-}
-}
-if ((num2 - 5) >= 0)
-{
-num1 = num1 + 1;
-num2 = num2 - 5;
-while (num2 >= 5)
-{
-num1 = num1 + 1;
-num2 = num2 - 5;
-}
-}
-if ((num2 - 2) >= 0)
-{
-num1 = num1 + 1;
-num2 = num2 - 2;
-while (num2 >= 2)
-{
-num1 = num1 + 1;
-num2 = num2 - 2;
-}
-}
-if ((num2 - 1) >= 0)
-{
-num1 = num1 + 1;
-num2 = num2 - 1;
 while (num2 >= 1)
 {
 num1 = num1 + 1;
+if (num2 >= 25)
+num2 = num2 - 25;
+else if (num2 >= 10)
+num2 = num2 - 10;
+else if (num2 >= 5)
+num2 = num2 - 5;
+else if (num2 >= 2)
+num2 = num2 - 2;
+else if (num2 >= 1)
 num2 = num2 - 1;
-}
 }
 printf("%d\n", num1);
 return (0);
