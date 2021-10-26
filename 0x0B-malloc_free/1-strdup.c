df282e43d7fad5decbe4eef51f72620c2d4a69ba
Santi3948 <santiagonegreyra@gmail.com>
2021-10-26 09:18:04 -0700

    strdup

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -10,13 +10,17 @@ char *_strdup(char *str)
 {
 	int i, n = 0, g;
 	char *d;
-
+	
+	if (str == NULL)
+        {
+        return (NULL);
+        }
 	for (i = 0; str[i]; i++)
 	n = n + 1;
 	d = malloc(sizeof(char) * n);
 	for (g = 0; g <= n - 1; g++)
 	d[g] = str[g];
-	if (str == NULL || d == NULL)
+	if (d == NULL)
 	{
 	return (NULL);
 	}
