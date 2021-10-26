b504cd4776ba5619512ca777e7ec840ae1b1433b
Santi3948 <santiagonegreyra@gmail.com>
2021-10-26 12:49:21 -0700

    alloc grid

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -22,7 +22,8 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	}
 	for (i = 0; i < height; i++)
-	{	for (j = 0; j < width)
+	{
+		for (j = 0; j < width)
 		{
 		d[i] = malloc(sizeof(int) * width);
 		}
