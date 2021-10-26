8b78f9f0bde024c53f60551711a1540a4fd01f8f
Santi3948 <santiagonegreyra@gmail.com>
2021-10-26 13:41:42 -0700

    see

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * free_grid - check the code
+ * @grid: str
+ * @height: dou
+ * Return: pointer to d.
+ */
+void free_grid(int **grid, int height)
+{	
+	int i;
+	for (i = 0; i < height; i++)
+	{
+		free(grid[i]);
+	}
+	free(grid);
+}
