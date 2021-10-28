ec730f0792f9e4e6528afe4c576cb7e314189519
Santi3948 <santiagonegreyra@gmail.com>
2021-10-28 14:53:51 -0700

    dou

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * array_range - writes the character c to stdout
+ * @min: The character to print
+ * @max: asd
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int *array_range(int min, int max)
+{
+	int *p, n;
+
+	if (min > max)
+	return (NULL)
+	p = malloc(sizeof(int) * (max - min + 1));
+	if (p == 0)
+	{
+	return (0);
+	}
+	for (n = 0; n < (max - min + 1); n++)
+	p[n] = n + min;
+	return (p);
+}
