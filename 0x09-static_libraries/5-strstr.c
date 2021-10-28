c5108b5913e1580a4ca1e70a303cd1b65b7f366f
Santi3948 <santiagonegreyra@gmail.com>
2021-10-27 19:15:49 -0700

    ss

--- 0x09-static_libraries/5-strstr.c
+++ 0x09-static_libraries/5-strstr.c
@@ -1,39 +1,20 @@
 #include "main.h"
-
 /**
- * _strstr -  function that gets the length of a prefix substring
- * @haystack: initial segment of
- * @needle: only of bytes from
- * Return: Returns the number of bytes in
+ * _strstr - locates a substring
+ * @haystack: pointer to string
+ * @needle: pointer substring
+ * Return: pointer to the beginning of substring
  */
 char *_strstr(char *haystack, char *needle)
 {
-int i = 0, j = 0, l = 0;
+	int larg, lar;
 
 
-	for (i = 0; needle[i]; i++)
-		l++;
-	if (l == 0)
-		return (haystack);
-
-	for (i = 0; haystack[i]; )
+	for (larg = 0; haystack[larg]; larg++)
 	{
-		while (needle[j] && haystack[i] == needle[0])
-		{
-			if (haystack[i + j] == needle[j])
-				j++;
-
-			else
-				break;
-			}
-
-		if (needle[j])
-		{
-			i++;
-			j = 0;
-		}
-		else
-			return (haystack + i);
 	}
+		for (lar = 0; needle[lar]; lar++)
+		{
+		}
 	return (0);
 }
