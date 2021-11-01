6e2fcfa619c0ac7ff3265c39c3b55cd282989178
Santi3948 <santiagonegreyra@gmail.com>
2021-11-01 11:29:49 -0700

    ds

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,16 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * struct dog - sc
+ * dog - qwedc
+ * @name: awdc
+ * @age: asd
+ * @owner: adad
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-} dog;
+}
 #endif
