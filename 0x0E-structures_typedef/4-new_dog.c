e17b223edcc67fae306c2d2a9232921d6c39aea6
Santi3948 <santiagonegreyra@gmail.com>
2021-11-01 14:59:59 -0700

    asdsd

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,6 +1,38 @@
 #include "dog.h"
-#include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
+/**
+  * _strdup - check the code
+  * @str: str
+  * Return: pointer to d.
+  */
+char *_strdup(char *str)
+{
+	int i, n = 0, g;
+	char *d;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+	for (i = 0; str[i]; i++)
+	n = n + 1;
+	d = malloc(sizeof(char) * n + 1);
+	if (d == NULL)
+	{
+		return (NULL);
+	}
+	for (g = 0; g <= n - 1; g++)
+	d[g] = str[g];
+	return (d);
+}
+/**
+ * new_dog - init
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: void
+ */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 dog_t *d;
