c1dc9b9988c0602ae5c2436631ed5db99fe850cb
Santi3948 <santiagonegreyra@gmail.com>
2021-11-03 08:09:10 -0700

    dssd

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "3-calc.h"
+/**
+ * array_iterator - print name
+ * @array: array
+ * @size: size
+ * @action: action
+ */
+int (*get_op_func(char *s))(int, int)
+{
+if ( s
+}
