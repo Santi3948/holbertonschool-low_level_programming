8663b493de3007313a55324a7191e95d60fdba1a
Santi3948 <santiagonegreyra@gmail.com>
2021-11-03 14:13:31 -0700

    sd

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,28 +1,31 @@
+#ifndef GET_OP_H
+#define GET_OP_H
 #include <stdio.h>
 #include <stdlib.h>
 #include "3-calc.h"
 /**
- * array_iterator - print name
- * @array: array
- * @size: size
- * @action: action
+ * get_op_func - print name
+ * @s: array
+ * @a: size
+ * @b: action
  */
 int (*get_op_func(char *s))(int a, int b)
 {
-    op_t ops[] = {
-        {"+", op_add},
-        {"-", op_sub},
-        {"*", op_mul},
-        {"/", op_div},
-        {"%", op_mod},
-        {NULL, NULL}
-    };
-    int i;
+	op_t ops[] = {
+	{"+", op_add},
+	{"-", op_sub},
+	{"*", op_mul},
+	{"/", op_div},
+	{"%", op_mod},
+	{NULL, NULL}
+	};
+	int i;
 
-if (s[0] != '+' && s[0] != '-' && s[0] != '*' && s[0] != '/' && s[0] != '%')
+if (s[0] != '+' && s[0] != '-' && s[0] != '*'
+&& s[0] != '/' && s[0] != '%' && !s[1])
 {
 printf("Error\n");
-exit (99);
+exit(99);
 }
 i = 0;
 while (i < 4 && ops[i].op[0] != s[0])
@@ -31,3 +34,4 @@ while (i < 4 && ops[i].op[0] != s[0])
 }
 return (ops[i].f);
 }
+#endif
