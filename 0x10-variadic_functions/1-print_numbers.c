c1dce44534fb846f6f48d8924c118919f88ace03
Santi3948 <santiagonegreyra@gmail.com>
2021-11-04 10:04:52 -0700

    sd

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -14,13 +14,15 @@ unsigned int i;
 
 va_start(ap, n);
 for (i = 0; i < n - 1 ; i++)
+{
 if (separator != NULL)
 {
 printf("%d%s", va_arg(ap, int), separator);
-{
+}
 else
 {
 	printf("%d", va_arg(ap, int));
+}
 }
 printf("%d\n", va_arg(ap, int));
 va_end(ap);
