7350a4d17370d8021d5ccfc2ca9322bb28d0d7b6
Santi3948 <santiagonegreyra@gmail.com>
2021-11-11 14:05:08 -0800

    dsds

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,10 @@
-#include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
-
+/**
+ * first - print before the main function
+ */
+void first(void) __attribute__((constructor));
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
