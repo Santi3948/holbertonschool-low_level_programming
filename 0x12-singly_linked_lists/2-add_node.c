e1b00818f491712760a3043dc504d3008dd367ec
Santi3948 <santiagonegreyra@gmail.com>
2021-11-11 08:18:40 -0800

    dou

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,10 +19,7 @@ list_t *add_node(list_t **head, const char *str)
 	}
 	new->str = strdup(str);
 	new->len = strlen(new->str);
-	if(*head)
-	{
 	new->next = *head
 	*head = new;
 	return (*head);
-	}
 }
