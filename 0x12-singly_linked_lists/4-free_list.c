2586bdc9011fe26777b7bf4e312d356b1f577583
Santi3948 <santiagonegreyra@gmail.com>
2021-11-11 08:07:34 -0800

    dou

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -2,11 +2,15 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stdio.h>
+/**
+ * free_list -  free all the elements of the list
+ * @head: the first element of the list
+ */
 void free_list(list_t *head)
 {
-        list_t *aux;
-	
-       	while (head)
+	list_t *aux;
+
+	while (head)
 	{
 	aux = head;
 	free(aux->str);
