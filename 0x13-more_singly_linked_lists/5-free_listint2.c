c151f350ca048a82e2ada1d03b689faa60efb0ba
Santi3948 <santiagonegreyra@gmail.com>
2021-11-16 06:08:15 -0800

    see

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * free_listint2 -  free all the elements of the list
+ * @head: the first element of the list
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *aux;
+
+	while (head)
+	{
+	aux = *head;
+	*head = (*head)->next;
+	free(aux);
+	}
+	head = NULL;
+}
