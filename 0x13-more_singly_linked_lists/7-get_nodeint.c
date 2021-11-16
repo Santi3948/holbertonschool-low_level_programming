72463faf0569b7ceba3097c2cfcb1d3c37f3b292
Santi3948 <santiagonegreyra@gmail.com>
2021-11-16 08:51:42 -0800

    dsdsd

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,18 +1,20 @@
 #include "lists.h"
 /**
- *
- *
+ * get_nodeint_at_index - druki
+ * @head: sdds
+ * @index: dss
+ * Return: list
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
 
-	while(head && i != index)
+	while (head && i != index)
 	{
 		i++;
 		head = head->next;
 	}
-	if(head)
+	if (head)
 		return (head);
 	else
 		return (NULL);
