c7252c51047c8e913fced22dc7975d26543668b2
Santi3948 <santiagonegreyra@gmail.com>
2021-11-18 08:51:57 -0800

    ds!

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -29,10 +29,10 @@ unsigned int binary_to_uint(const char *b)
 
 	for (i = 0; b[i]; i++)
 	{
-		if (b[len] != '0' && b[len] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 	}
-
+	i--;
 	for (; i >= 0; i--, j++)
 	{
 		if (b[i] == '1')
