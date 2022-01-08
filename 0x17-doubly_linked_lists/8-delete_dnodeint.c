8a81972033c8f3cbecfe917423cad40142105e30
Santi3948 <santiagonegreyra@gmail.com>
2022-01-08 13:06:05 -0800

    fixing 8

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * dlistint_len -  returns the number of elements in a linked dlistint_t list
+ * @h: the list
+ * Return: number of elements in a linked
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
+/**
  * delete_dnodeint_at_index - deletes the node at index
  * @head: the list
  * @index: is the index of the node that should be deleted. Index starts at 0
