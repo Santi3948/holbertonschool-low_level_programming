408bd313b055e3e199ccb7d6cf691ee0bacbd401
Santi3948 <santiagonegreyra@gmail.com>
2022-01-08 14:40:37 -0800

    fixing 7!

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -31,7 +31,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (!h || !aux2 || idx > size)
 		return (NULL);
 	if (idx == 0)
-                add_dnodeint(h, n);
+		add_dnodeint(h, n);
 	else if (idx == size)
 		add_dnodeint_end(h, n);
 	else
