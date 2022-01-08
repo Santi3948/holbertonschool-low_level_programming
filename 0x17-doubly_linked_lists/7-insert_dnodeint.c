4f241d110bf7208568a6462012c05f598db9d475
Santi3948 <santiagonegreyra@gmail.com>
2022-01-08 13:03:03 -0800

    fixing 7

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -2,6 +2,19 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
+ * dlistint_len -  returns the number of elements in a linked dlistint_t list
+ * @h: the list
+ * Return: number of elements in a linked
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	int i;
+
+	for (i = 0; h; i++)
+		h = h->next;
+	return (i);
+}
+/**
  * insert_dnodeint_at_index - inserts a new node at a given position.
  * @h: the list
  * @idx: is the index of the list where the new node should be added.
